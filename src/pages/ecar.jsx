2bb7b060862c7e0cbeefe2b4fa834e547328a8e0
Phaako <faako.edward@gmail.com>
2025-02-16 23:10:46 +0000

    some edits

--- src/pages/ecar.jsx
+++ src/pages/ecar.jsx
@@ -24,11 +24,11 @@ const ecar = () => {
                   <div className='flex justify-center text-center items-center text-xl w-[90vw] md:w-[40vw] mx-auto'>
                     <p>
             
-                    As the Lead Senior CAD Engineer, I led the design of Cariso, an electric multi-purpose vehicle (MPV) engineered specifically for Ghanaian roads and markets. This project aims to tackle transportation challenges, providing a sustainable and affordable mobility solution to help reduce poverty and enhance economic opportunities. Designed with durability, efficiency, and local adaptability in mind, Cariso is a step toward a greener and more inclusive future.
+                    As the Lead Senior CAD Engineer, I led the design of an electric Multi-Purpose Vehicle (eMPV), engineered specifically for Ghanaian roads and markets. This project aims to tackle transportation challenges, providing a sustainable and affordable mobility solution to help reduce poverty and enhance economic opportunities. Designed with durability, efficiency, and local adaptability in mind, the eMPV is a step toward a greener and more inclusive future.
                     </p>
                   </div>
             <div className='flex md:pl-44 space-x-4'>
-                <h1 className='text 4xl flex justify-start items-center pt-4 font-semibold'> Design and Build Process in pictures </h1>
+                <h1 className='border w-[60vw] text-center md:w-[30vw] md:text-center text-xl pt-2 h-[6vh] rounded-full md:ml-44 bg-[#1C1C1C] text-white shadow-2xl'> Design and Build Process in pictures </h1>
             </div>
                   <div className='md:flex flex-grow w-[65vw]  md:w-[40vw] ml-30 md:m-[9rem] mt-[5rem] space-y-6 md:space-x-6'>
                     <img className='rounded-3xl   shadow-black' src={Cariso}  />
@@ -59,7 +59,7 @@ const ecar = () => {
                     <img className='w-[800px] h-[400px] rounded-3x1 shadow-black' src={carside}/>
                   </div>
                   <div className='flex flex-col text-center md:m-0 m-5 space-y-4'>
-                    <h1 className='border w-[30vw] text-center md:w-[10vw] md:text-center text-xl pt-2 h-[8vh] rounded-full md:ml-44 bg-[#1C1C1C] text-white shadow-2xl'>Details</h1>
+                    <h1 className='border w-[30vw] text-center md:w-[10vw] md:text-center text-xl pt-2 h-[6vh] rounded-full md:ml-44 bg-[#1C1C1C] text-white shadow-2xl'>Details</h1>
             </div>
 
                     <div className='flex md:pl-44 space-x-2'>
@@ -88,7 +88,7 @@ WeCar is more than just a vehicle—it’s a solution for mobility and economic 
    </div>  
   <div>
     <section className="py-16 px-4 flex flex-col items-center">
-      <h2 className="text-3xl font-bold mb-4">WeCar in Action-first test</h2>
+      <h2 className="border w-[30vw] text-center md:w-[30vw] md:text-center text-xl pt-2 h-[6vh] rounded-full md:ml-44 bg-[#1C1C1C] text-white shadow-2xl">WeCar in Action-first test</h2>
       <p className="text-gray-600 mb-6 text-center">
         Watch how our ideas are brought to life.
       </p>
